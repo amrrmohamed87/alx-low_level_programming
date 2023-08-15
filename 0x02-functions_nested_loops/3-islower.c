7154280eb759bf1da165ebfa423885c466ee2b06
amrrmohamed87 <me@example.com>
2023-08-15 16:29:37 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _isalpha - Checks for alphabetic order
- *
- * @c: alpabet to check for
- *
- * Return: 1 if c is a letter, 0 otherwise
+ * _islower - Checks for lowercase alphabet
+ * @c: Character to be checked
+ * Return: if character is lowercase, otherwise
  */
 
-int _isalpha(int c)
+int _islower(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
 
