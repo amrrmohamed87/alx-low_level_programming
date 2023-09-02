b2b7f3fc0ec204071f5bd14a811cca2a0688ac3c
amrrmohamed87 <me@example.com>
2023-09-01 17:12:49 -0700

    libmy

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,22 +1,16 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
  * _puts - function that prints strings on the output screen
- * @str: input value
+ * @s: input value
  *
  * Return: void
  */
-void _puts(char *str);
-#include<stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts(s);
 }
-
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,5 +1,8 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 /**
  * _putchar - writes the character c to output
@@ -9,5 +12,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (putchar(c));
 }
