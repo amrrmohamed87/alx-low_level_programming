f8d9ee595b5294e53c46582a96f9ecbe3c3150b4
amrrmohamed87 <me@example.com>
2023-09-01 17:53:27 -0700

    libmy

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints string on output screen
+ * @str: input value
+ */
+void _puts(char *str);
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
